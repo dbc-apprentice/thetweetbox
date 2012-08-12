8db0fd110cde95209f52b5245dd7ad9a0ecb7ca0
June Jung <apprentice@devbootcamp.com>
2012-08-12 16:37:27 -0700

    deleted testing with twitter api

--- spec/lib/twitter_client_spec.rb
+++ spec/lib/twitter_client_spec.rb
@@ -2,19 +2,4 @@ require 'spec_helper'
 
 describe TwitterClient do
 
-  context "gets all the tweets with urls for the user" do
-    let(:user) do
-      { auth_token: "398173256-atW28FF5XpkglQMkypIqfgJGqMDs1L4OnnQqaPtR",
-        auth_secret: "97pfLOOgODzW0KAfEinBbp6Lc2qX9UQ6hvgpf9ANsbI",
-        twitter_name: "NatashaTheRobot"
-      }
-    end
-
-    it "returns all tweets with urls" do
-      tweets = TwitterClient.get_tweets_with_urls(user)
-      tweets.first["urls"]["entities"].should_not be_empty
-    end
-
-  end
-
 end
