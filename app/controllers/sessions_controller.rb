59ad9f1997b6c8bb5733d358a59629dcd9fb463f
NatashaMurashev <nmurashev@gmail.com>
2012-08-13 15:42:51 -0700

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    TwitterClient.get_user_tweets(user)
+    TwitterClient.get_tweets(user)
     redirect_to root_url, :notice => "Signed in!"
   end
 
