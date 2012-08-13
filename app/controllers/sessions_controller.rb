b3c467536d42ab1d6b0b7ec8cccbfe4951d7dbdd
NatashaMurashev <nmurashev@gmail.com>
2012-08-13 12:57:44 -0700

    updated routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    TwitterClinet.get_user_tweets(user)
     redirect_to root_url, :notice => "Signed in!"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Thetweetbox::Application.routes.draw do
   root to: "users#new"
   resources :users, except: [:show]
   get "/auth/twitter/callback" => "sessions#create"
-  get "/:twitter_name" => "users#show", :as => 'user'
   get "/signout" => "sessions#destroy", :as => :signout
+  get "/:twitter_name" => "users#show", :as => "user"
 
 end
 
