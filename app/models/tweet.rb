dc7069574ed55bf9c55f47242a394a9443e25f0d
June Jung <apprentice@devbootcamp.com>
2012-08-13 10:23:37 -0700

    updated tweet and user model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -17,31 +17,31 @@ class Tweet < ActiveRecord::Base
   has_and_belongs_to_many :links
 
   def self.create_from_twitter(twitter_tweet)
-    unless twitter_tweet["entities"]["urls"].empty?
-      unless Tweet.find_by_twitter_id(twitter_tweet["id"].to_i)
-        last_tweet = create! do |tweet|
-          tweet.user = twitter_tweet[:user]
-          tweet.twitter_id = twitter_tweet["id"].to_i
-          tweet.text = CGI.escape(twitter_tweet["text"])
-        end
 
-        #create a new link
-        twitter_tweet["entities"]["urls"].each do |url|
-          url[:tweet] = last_tweet
-          Link.create_from_url(url)
-        end
-
-
-        #create a new hashtag if there are any
-        unless twitter_tweet["entities"]["hashtags"].empty?
-          twitter_tweet["entities"]["hashtags"].each do |hashtag|
-            hashtag[:tweet] = last_tweet
-            Hashtag.create_from_tweet(hashtag)
-          end
-        end
-
+    unless Tweet.find_by_twitter_id(twitter_tweet["id"].to_i)
+      last_tweet = create! do |tweet|
+        tweet.user = twitter_tweet[:user]
+        tweet.twitter_id = twitter_tweet["id"].to_i
+        tweet.text = CGI.escape(twitter_tweet["text"])
       end
-    end
 
+      #create a new link
+      twitter_tweet["entities"]["urls"].each do |url|
+        url[:tweet] = last_tweet
+        Link.create_from_url(url)
+      end
+
+
+      #create a new hashtag if there are any
+      unless twitter_tweet["entities"]["hashtags"].empty?
+        twitter_tweet["entities"]["hashtags"].each do |hashtag|
+          hashtag[:tweet] = last_tweet
+          Hashtag.create_from_tweet(hashtag)
+        end
+      end
+
+    end
   end
+
+
 end
