1ba300b0017dc62ef8bc6ed6976e51ced1eb0d53
NatashaMurashev <nmurashev@gmail.com>
2012-08-15 11:29:17 -0700

    now sorting tweets by tweet id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(:first, :conditions => ["lower(twitter_name) =?", params[:twitter_name].downcase])
-    @tweets = @user.tweets.order("created_at ASC").scoped.page(params[:page]).per(50)
+    @tweets = @user.tweets.order("twitter_id DESC").scoped.page(params[:page]).per(50)
     hashtags = []
     @user.tweets.each do |tweet|
       tweet.hashtags.each do |hashtag|
